32c3c6651c8fe78ff5b16d52120463e0442ec076
NEFEDOV_IT <nefedov_it@mail.ru>
2021-12-17 01:15:43 +0300

    fix bugs and add new in isEmptyCity and remove city in function

--- main.js
+++ main.js
@@ -7,22 +7,22 @@ function cloudResult() {
     const input = document.querySelector('.forest__input')
     let city = input.value
     const url = `${URL.server}?q=${city}&appid=${URL.apiKey}`
-    const isEmptyFirstName = /^[a-zA-Z]+$/.test(city.trim())
+    const isEmptyCity = /^[a-zA-Z ]+$/.test(city.trim())
 
     try {
         input.classList.remove('error')
-        if (!isEmptyFirstName) throw new SyntaxError("City is not entered or entered incorrectly, check the correctness of the entry.")
+        if (!isEmptyCity) throw new SyntaxError("City is not entered or entered incorrectly, check the correctness of the entry.")
     } catch (e) {
         UI_ELEMENTS.form.reset()
         input.classList.add('error')
         return alert('The data is incomplete: ' + e.message)
     }
 
-    city = capitalLetter(city)
+    capitalLetter(city)
 
 fetch(url)
     .then(response => {
-        return response.ok ? response.json() : alert("Ошибка HTTP: " + response.status + `\nГород не найден!`);
+        return response.ok ? response.json() : alert("Ошибка HTTP: " + response.status + `\nГород не найден!`)
     })
     .then(function (answer) {
         UI_ELEMENTS.cities.forEach(item => item.textContent = answer.name)
@@ -46,10 +46,8 @@ function capitalLetter(city) {
 
 const tabs_items = document.querySelectorAll('.tabs-item')
 const tabs_blocks = document.querySelectorAll('.tabs-block')
-
 for (let i = 0; i < tabs_items.length; i++) {
     const tabs_item = tabs_items[i]
-
     tabs_item.addEventListener('click', () => {
         for (let i = 0; i < tabs_items.length; i++) {
             const tabs_item = tabs_items[i]
